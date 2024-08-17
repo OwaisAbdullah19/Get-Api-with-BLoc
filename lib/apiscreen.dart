1fea6949d4f844282c937d9fb5f57a1cd5f6e0d0
Owais Abdullah <owaisabdullah19@gmail.com>
2024-08-17 22:35:01 +0500

    Textformfield

--- lib/apiscreen.dart
+++ lib/apiscreen.dart
@@ -16,28 +16,48 @@ class _ApiscreenState extends State<Apiscreen> {
     super.initState();
     context.read<GetApiBloc>().add(fetchdata());
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('Api Data')),
-      ),
-      body:BlocBuilder<GetApiBloc, GetApiState>(
-        builder: (context, state) {
-          return ListView.builder(
-            itemCount: state.postmodellist.length,
-            itemBuilder: (context,index){
-              final item = state.postmodellist[index];
-              return ListTile(
-                title: Text(item.title.toString()),
-                subtitle: Text(item.body.toString()),
-              );
-            }
-            
+        appBar: AppBar(
+          title: Center(child: Text('Api Data')),
+        ),
+        body: BlocBuilder<GetApiBloc, GetApiState>(
+          builder: (context, state) {
+            return Column(
+              children: [
+                Padding(
+                  padding: EdgeInsetsDirectional.all(8),
+                  child: TextFormField(
+                    decoration: InputDecoration(hintText: 'Search  '),
+                    onChanged: (value) {
+
+                      
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                      itemCount: state.postmodellist.length,
+                      itemBuilder: (context, index) {
+                        final item = state.postmodellist[index];
+                        return ListTile(
+                          isThreeLine: true,
+                          title: Text('ID ' + item.id.toString()),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(item.title.toString()),
+                              Text(item.body.toString()),
+                            ],
+                          ),
+                        );
+                      }),
+                )
+              ],
             );
-        },
-      ) 
-      
-    );
+          },
+        ));
   }
 }
